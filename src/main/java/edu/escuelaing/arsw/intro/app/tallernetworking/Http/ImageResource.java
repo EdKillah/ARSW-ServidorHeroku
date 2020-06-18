9ef8fd85b004ef0f0a627e6d7ab354cbac4df396
EdKillah <eduard.jimenez@mail.escuelaing.edu.co>
2020-06-18 14:57:13 -0500

    sextoa commitq

--- src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
+++ src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
@@ -23,16 +23,17 @@ public class ImageResource {
         System.out.println("entra?????? ****final: "+archivoEncontrado);
         System.out.println("ME ENCUENTRO EN IMAGEN Y ESTA ES LA RES: "+res);
 		String archivoNombre =  archivoEncontrado.toString().replace(res, "");
-		File f = new File(archivoNombre+"/img/"+res);
+		File f = new File(archivoNombre+"img/"+res);
         if(res.contains("img/")){
             
             res = res.substring(4,res.length());
             System.out.println("Entro en condicAICIOn: "+res);
         }
 		System.out.println("Questa e la imagine: "+f.toString());
+		
         //BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + "/resources" + "/suzaku.PNG"));
         //BufferedImage image = ImageIO.read(new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\"+res)); 
-		BufferedImage image = ImageIO.read(new File(archivoNombre+"/img/"+res)); 
+		BufferedImage image = ImageIO.read(new File(archivoNombre+"img/"+res)); 
         //BufferedImage image = ImageIO.read(archivoEncontrado);
         //new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\bugs.jpg")
                     //C:\Users\Z470\Documents\NetBeansProjects\TallerNetworking\src\main\resources\img\suzaku.png
