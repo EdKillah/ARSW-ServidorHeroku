e687dd97be0a4085852b85b2fe485357855ee799
EdKillah <eduard.jimenez@mail.escuelaing.edu.co>
2020-06-18 13:37:36 -0500

    sextoa commit

--- src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
+++ src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
@@ -30,7 +30,7 @@ public class ImageResource {
         }
         //BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + "/resources" + "/suzaku.PNG"));
         //BufferedImage image = ImageIO.read(new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\"+res)); 
-		BufferedImage image = ImageIO.read(new File(archivoNombre+"img/"+res)); 
+		BufferedImage image = ImageIO.read(new File(archivoNombre+"/img/"+res)); 
         //BufferedImage image = ImageIO.read(archivoEncontrado);
         //new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\bugs.jpg")
                     //C:\Users\Z470\Documents\NetBeansProjects\TallerNetworking\src\main\resources\img\suzaku.png
