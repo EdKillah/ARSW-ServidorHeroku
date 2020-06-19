662e1582e7a50211ed25c0c1c8f13b4e88be8831
EdKillah <eduard.jimenez@mail.escuelaing.edu.co>
2020-06-18 21:16:56 -0500

    cambio

--- src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
+++ src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
@@ -21,7 +21,6 @@ public class ImageResource {
     public void drawImage(OutputStream  clientSocket,PrintWriter out ,String res, File archivoEncontrado) throws IOException {
         
         System.out.println("entra?????? ****final: "+archivoEncontrado);
-        System.out.println("ME ENCUENTRO EN IMAGEN Y ESTA ES LA RES: "+res);
 		String archivoNombre =  archivoEncontrado.toString().replace(res, "");
 		System.out.println("ME ENCUENTRO EN IMAGEN Y ESTA ES LA RES: "+res);
 		System.out.println("archivoNombre: "+archivoNombre);
@@ -31,11 +30,7 @@ public class ImageResource {
             res = res.substring(4,res.length());
             System.out.println("Entro en condicAICIOn: "+res);
         }
-		//if(f==null){
-		//	System.out.println("ES NULO ESTA PORQUERIA");
-		//}
-		//else{
-		//	String axa=f.toString();
+
 		File f = new File("app/src/main/resources/img/suzaku.png");
 		if(f==null){
 			System.out.println("Y TAN LARGO EL RENCOR");
@@ -46,14 +41,14 @@ public class ImageResource {
 		}
 		//}
 		
-        //BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + "/resources" + "/suzaku.PNG"));
+        BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + "/resources" + "/suzaku.PNG"));
         //BufferedImage image = ImageIO.read(new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\"+res)); 
 		//BufferedImage image = ImageIO.read(new File(archivoNombre+"img/"+res)); 
 		  //BufferedImage image = ImageIO.read(new File("/app/src/main/resources/img/suzaku.png")); 
 		//BufferedImage image = ImageIO.read(f); 
 		
 		
-		BufferedImage image = ImageIO.read(new File("/app/src/main/resources/img/bugs.jpg")); 
+		//BufferedImage image = ImageIO.read(new File("/app/src/main/resources/img/bugs.jpg")); 
 		
 		
         //BufferedImage image = ImageIO.read(archivoEncontrado);
@@ -65,7 +60,7 @@ public class ImageResource {
         System.out.println("la imagen SE MOSTRARAA: "+xxxx);
 		out.println("HTTP/1.1 200 OK");		
         out.println("Content-Type: image/png");
-		out.println("Content-Length: " + 442);
+		//out.println("Content-Length: " + 442);
 		out.println();
         //out.print("HTTP/1.1 200 OK \r\n");
         //out.print("Content-Type: image/png \r\n");
