fd0989eec360b4566580e58cbe197ff21eff9f5e
EdKillah <eduard.jimenez@mail.escuelaing.edu.co>
2020-06-18 21:28:05 -0500

    cambios

--- src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
+++ src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/ImageResource.java
@@ -20,52 +20,15 @@ public class ImageResource {
 
     public void drawImage(OutputStream  clientSocket,PrintWriter out ,String res, File archivoEncontrado) throws IOException {
         
-        System.out.println("entra?????? ****final: "+archivoEncontrado);
-		String archivoNombre =  archivoEncontrado.toString().replace(res, "");
-		System.out.println("ME ENCUENTRO EN IMAGEN Y ESTA ES LA RES: "+res);
-		System.out.println("archivoNombre: "+archivoNombre);
-		//File f = new File(archivoNombre+"img/"+res);
-        if(res.contains("img/")){
-            
-            res = res.substring(4,res.length());
-            System.out.println("Entro en condicAICIOn: "+res);
-        }
-
-		File f = new File("app/src/main/resources/img/suzaku.png");
-		if(f==null){
-			System.out.println("Y TAN LARGO EL RENCOR");
-		}
-		else{
-			System.out.println("Questa e la imagine: "+f.toString());
-			System.out.println("Questa e la imagine: "+f);
-		}
-		//}
-		
-        //BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + "/resources" + "/suzaku.PNG"));
-        //BufferedImage image = ImageIO.read(new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\"+res)); 
-		//BufferedImage image = ImageIO.read(new File(archivoNombre+"img/"+res)); 
-		  //BufferedImage image = ImageIO.read(new File("/app/src/main/resources/img/suzaku.png")); 
-		//BufferedImage image = ImageIO.read(f); 
-		
-		
-		BufferedImage image = ImageIO.read(new File("/app/src/main/resources/img/bugs.jpg")); 
-		
-		
-        //BufferedImage image = ImageIO.read(archivoEncontrado);
-        //new File("C:\\Users\\Z470\\Documents\\NetBeansProjects\\TallerNetworking\\src\\main\\resources\\img\\bugs.jpg")
-                    //C:\Users\Z470\Documents\NetBeansProjects\TallerNetworking\src\main\resources\img\suzaku.png
-        //System.out.println("PASA EL PRIMER LINEA");
-		//out.println("Archivoo: "+archivoNombre);
-		String xxxx=  image.toString();
-        System.out.println("la imagen SE MOSTRARAA: "+xxxx);
-		out.println("HTTP/1.1 200 OK");		
-        out.println("Content-Type: image/png");
-		out.println("Content-Length: " + 256180);
-		out.println();
-        //out.print("HTTP/1.1 200 OK \r\n");
-        //out.print("Content-Type: image/png \r\n");
-        //out.print("\r\n\r\n");
-        ImageIO.write(image, "PNG", clientSocket);
+			BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir")+ "/resources/" + res));
+            ByteArrayOutputStream ArrBytes = new ByteArrayOutputStream();
+            DataOutputStream writeImg = new DataOutputStream(clientSocket);
+            ImageIO.write(image, "PNG", ArrBytes);
+            writeImg.writeBytes("HTTP/1.1 200 OK \r\n");
+            writeImg.writeBytes("Content-Type: image/png \r\n");
+            writeImg.writeBytes("\r\n");
+            writeImg.write(ArrBytes.toByteArray());
+            System.out.println(System.getProperty("user.dir") + res);
         
     }
 }
