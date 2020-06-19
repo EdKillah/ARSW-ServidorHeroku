f889dc48e07e17087ba4a42e657118c149d69d91
EdKillah <eduard.jimenez@mail.escuelaing.edu.co>
2020-06-18 23:26:37 -0500

    esta esaza

--- src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/HttpServer.java
+++ src/main/java/edu/escuelaing/arsw/intro/app/tallernetworking/Http/HttpServer.java
@@ -136,7 +136,7 @@ public class HttpServer implements Runnable {
     private File buscarArchivo(String res) {
         BuscarArchivo find = new BuscarArchivo();
         //return new File(System.getProperty("user.dir") + "\\src\\main\\resources\\" + res); //"index.html"
-		System.out.println("find: "+find.buscador(res,System.getProperty("user.dir")));
+		System.out.println("find: "+find.buscador(res,new File(System.getProperty("user.dir"))));
     	//System.out.println("BuscandoArchivos: "+System.getProperty("user.dir")+" esto me pasaron: "+res);
 		return new File(System.getProperty("user.dir") + "/src/main/resources/" + res); //"index.html"
 		//return new File("/src/main/resources/"+res);
